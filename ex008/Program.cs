0b0db1d720fff047181b854775cc44a451ae43bc
GUSTAVO DE ALMEIDA GIUZIO <gualmeida81@gmail.com>
2025-01-15 22:11:15 -0300

    atz

---
+++ ex008/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Digite a velocidade do carro: ");
+double velocidade = double.Parse(s: Console.ReadLine());
+
+if (velocidade > 80)
+{
+    double excesso = velocidade - 80;
+    double multa = excesso * 7;
+    Console.WriteLine($"você foi multado, o valor da multa é R${multa:f2}");
+}
+else
+{
+    Console.WriteLine("você está dentro do limite de velocidade.");
+
+}
