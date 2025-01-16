4dbb70e432868fa70c00aebc785ad7b2d48ae939
GUSTAVO DE ALMEIDA GIUZIO <gualmeida81@gmail.com>
2025-01-15 21:27:59 -0300

    atualização do ex

---
+++ ex005/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double nota1, nota2, media;
+
+            
+            Console.Write("primeira nota: ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+
+            
+            Console.Write("segunda nota: ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+
+            
+            media = (nota1 + nota2) / 2;
+
+            
+            Console.WriteLine($"A média do aluno é: {media:F2}");
+        }
+    }
+}
---
+++ ex007/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ex007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota_final;
+            string resultado;
+
+            Console.Write("Digite a nota final do aluno: ");
+            nota_final = Convert.ToDouble(Console.ReadLine());
+
+            if (nota_final > 70)
+            {
+                resultado = "aproved";
+                if (nota_final >= 95)
+                {
+                    resultado = "aprovado com louvo";
+                }
+               
+            }
+            else if (nota_final >= 45)
+            {
+                resultado = "em recuperação";
+            }
+               else   
+            {
+                resultado = "reproved";
+                    
+               }
+            Console.WriteLine("Nota do aluno {0} - resultado  {1}", nota_final, resultado);
+        }
+    }
+}
